cc0fd0a5addb15e0e857662f4d813d43c946ac9b
ROBERT TWELVES <ROBERTTWELVES@exe-coll.ac.uk>
2023-10-30 14:52:06 +0000

    new timer

--- Mastermind-v2/Military.Designer.cs
+++ Mastermind-v2/Military.Designer.cs
@@ -29,17 +29,30 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Military));
             this.countdownTimer = new System.Windows.Forms.Label();
             this.timer = new System.Windows.Forms.Timer(this.components);
+            this.purpleDot = new System.Windows.Forms.PictureBox();
+            this.blueDot = new System.Windows.Forms.PictureBox();
+            this.greenDot = new System.Windows.Forms.PictureBox();
+            this.yellowDot = new System.Windows.Forms.PictureBox();
+            this.orangeDot = new System.Windows.Forms.PictureBox();
+            this.redDot = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.purpleDot)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blueDot)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greenDot)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yellowDot)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.orangeDot)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redDot)).BeginInit();
             this.SuspendLayout();
             // 
             // countdownTimer
             // 
             this.countdownTimer.BackColor = System.Drawing.Color.LemonChiffon;
             this.countdownTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.countdownTimer.Location = new System.Drawing.Point(292, 30);
+            this.countdownTimer.Location = new System.Drawing.Point(277, 30);
             this.countdownTimer.Name = "countdownTimer";
-            this.countdownTimer.Size = new System.Drawing.Size(90, 44);
+            this.countdownTimer.Size = new System.Drawing.Size(105, 44);
             this.countdownTimer.TabIndex = 0;
             this.countdownTimer.Text = "000";
             this.countdownTimer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -49,16 +62,101 @@
             this.timer.Enabled = true;
             this.timer.Interval = 1000;
             // 
+            // purpleDot
+            // 
+            this.purpleDot.BackColor = System.Drawing.Color.Transparent;
+            this.purpleDot.BackgroundImage = global::Mastermind_v2.Properties.Resources.purple_circle_removebg_preview;
+            this.purpleDot.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.purpleDot.Location = new System.Drawing.Point(338, 706);
+            this.purpleDot.Name = "purpleDot";
+            this.purpleDot.Size = new System.Drawing.Size(57, 56);
+            this.purpleDot.TabIndex = 11;
+            this.purpleDot.TabStop = false;
+            this.purpleDot.UseWaitCursor = true;
+            // 
+            // blueDot
+            // 
+            this.blueDot.BackColor = System.Drawing.Color.Transparent;
+            this.blueDot.BackgroundImage = global::Mastermind_v2.Properties.Resources.blue_circle_removebg_preview;
+            this.blueDot.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.blueDot.Location = new System.Drawing.Point(277, 706);
+            this.blueDot.Name = "blueDot";
+            this.blueDot.Size = new System.Drawing.Size(57, 56);
+            this.blueDot.TabIndex = 10;
+            this.blueDot.TabStop = false;
+            this.blueDot.UseWaitCursor = true;
+            // 
+            // greenDot
+            // 
+            this.greenDot.BackColor = System.Drawing.Color.Transparent;
+            this.greenDot.BackgroundImage = global::Mastermind_v2.Properties.Resources.green_circle_removebg_preview;
+            this.greenDot.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.greenDot.Location = new System.Drawing.Point(214, 706);
+            this.greenDot.Name = "greenDot";
+            this.greenDot.Size = new System.Drawing.Size(57, 56);
+            this.greenDot.TabIndex = 9;
+            this.greenDot.TabStop = false;
+            this.greenDot.UseWaitCursor = true;
+            // 
+            // yellowDot
+            // 
+            this.yellowDot.BackColor = System.Drawing.Color.Transparent;
+            this.yellowDot.BackgroundImage = global::Mastermind_v2.Properties.Resources.yellow_circle_removebg_preview;
+            this.yellowDot.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.yellowDot.Location = new System.Drawing.Point(155, 706);
+            this.yellowDot.Name = "yellowDot";
+            this.yellowDot.Size = new System.Drawing.Size(57, 56);
+            this.yellowDot.TabIndex = 8;
+            this.yellowDot.TabStop = false;
+            this.yellowDot.UseWaitCursor = true;
+            // 
+            // orangeDot
+            // 
+            this.orangeDot.BackColor = System.Drawing.Color.Transparent;
+            this.orangeDot.BackgroundImage = global::Mastermind_v2.Properties.Resources.orange_circle_removebg_preview__2_;
+            this.orangeDot.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.orangeDot.Location = new System.Drawing.Point(93, 706);
+            this.orangeDot.Name = "orangeDot";
+            this.orangeDot.Size = new System.Drawing.Size(57, 56);
+            this.orangeDot.TabIndex = 7;
+            this.orangeDot.TabStop = false;
+            this.orangeDot.UseWaitCursor = true;
+            // 
+            // redDot
+            // 
+            this.redDot.BackColor = System.Drawing.Color.Transparent;
+            this.redDot.BackgroundImage = global::Mastermind_v2.Properties.Resources.red_circle_removebg_preview;
+            this.redDot.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.redDot.Location = new System.Drawing.Point(30, 706);
+            this.redDot.Name = "redDot";
+            this.redDot.Size = new System.Drawing.Size(57, 56);
+            this.redDot.TabIndex = 6;
+            this.redDot.TabStop = false;
+            this.redDot.UseWaitCursor = true;
+            // 
             // Military
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Mastermind_v2.Properties.Resources.mastermind_pic1;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(438, 830);
+            this.ClientSize = new System.Drawing.Size(423, 830);
+            this.Controls.Add(this.purpleDot);
+            this.Controls.Add(this.blueDot);
+            this.Controls.Add(this.greenDot);
+            this.Controls.Add(this.yellowDot);
+            this.Controls.Add(this.orangeDot);
+            this.Controls.Add(this.redDot);
             this.Controls.Add(this.countdownTimer);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Military";
             this.Text = "Military";
+            ((System.ComponentModel.ISupportInitialize)(this.purpleDot)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blueDot)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greenDot)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yellowDot)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.orangeDot)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redDot)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -67,5 +165,11 @@
 
         private System.Windows.Forms.Label countdownTimer;
         private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.PictureBox purpleDot;
+        private System.Windows.Forms.PictureBox blueDot;
+        private System.Windows.Forms.PictureBox greenDot;
+        private System.Windows.Forms.PictureBox yellowDot;
+        private System.Windows.Forms.PictureBox orangeDot;
+        private System.Windows.Forms.PictureBox redDot;
     }
 }
--- Mastermind-v2/Military.cs
+++ Mastermind-v2/Military.cs
@@ -14,17 +14,18 @@ namespace Mastermind_v2
     {
         static void DisplayArray(string[] arr) => Console.WriteLine(string.Join(" ", arr));
 
-        int seconds;
         public Military()
         {
             InitializeComponent();
-
-            seconds = Convert.ToInt32(10);
-            timer.Start();
         }
 
         private void Military_Load(object sender, EventArgs e)
         {
+            Timer MyTimer = new Timer();
+            MyTimer.Interval = (8 * 60 * 1000); // 8 mins
+            MyTimer.Tick += new EventHandler(timer_Tick);
+            MyTimer.Start();
+
             string[] colourArray = { "red", "orange", "yellow", "green", "blue", "purple" };
             Random rondo = new Random();
             string[] answer = new string[5];
@@ -33,7 +34,7 @@ namespace Mastermind_v2
             {
                 int num = rondo.Next(0, 6);
                 answer[i] = colourArray[num];
-                //   
+                 
             }
             DisplayArray(answer);
 
@@ -41,15 +42,11 @@ namespace Mastermind_v2
 
         private void timer_Tick(object sender, EventArgs e)
         {
-            countdownTimer.Text = seconds--.ToString();
-            if (seconds < 0)
-            {
-                timer.Stop();
-                gameOver newform = new gameOver();
-                this.Hide();
-                newform.ShowDialog();
-                this.Show();
-            }
+            MessageBox.Show("The form will now be closed.", "Time Elapsed");
+            gameOver newform = new gameOver();
+            this.Close();
+            newform.ShowDialog();
+            
         }
     }
 }
